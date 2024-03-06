a68b027085653eb9dc04f05021b334e78a5e9c7c
ASDomoroschenov <sashachicha228@mail.ru>
2024-03-07 01:45:57 +0300

    finish

--- JavaCipherService/JavaBitPermutationService/src/main/java/ru/mai/encryption/encryption_interface/Encryption.java
+++
@@ -1,5 +0,0 @@
-package ru.mai.encryption.encryption_interface;
-
-public interface Encryption {
-    byte[] encrypt(byte[] bytes, byte[] roundKey);
-}
--- JavaCipherService/JavaBitPermutationService/src/main/java/ru/mai/encryption/encryption_interface/SymmetricCipher.java
+++
@@ -1,7 +0,0 @@
-package ru.mai.encryption.encryption_interface;
-
-public interface SymmetricCipher {
-    byte[] encrypt(byte[] bytes);
-
-    byte[] decipher(byte[] bytes);
-}
