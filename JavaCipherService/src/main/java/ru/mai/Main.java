d8acfdca1502525ed7879901ef77f6799108eb92
ASDomoroschenov <sashachicha228@mail.ru>
2024-03-07 01:58:16 +0300

    refactor code

--- JavaCipherService/src/main/java/ru/mai/Main.java
+++ JavaCipherService/src/main/java/ru/mai/Main.java
@@ -1,7 +1,6 @@
 package ru.mai;
 
 import lombok.extern.slf4j.Slf4j;
-import ru.mai.utils.BitsUtil;
 import ru.mai.utils.BytesUtil;
 
 import java.util.Arrays;
@@ -9,8 +8,8 @@ import java.util.Arrays;
 @Slf4j
 public class Main {
     public static void main(String[] args) {
-        byte[] arrayBits = new byte[] {1};
-        int[] permutationValues = new int[] {8, 8, 8};
+        byte[] arrayBits = new byte[]{1};
+        int[] permutationValues = new int[]{8, 8, 8};
 
         try {
             byte[] resultPermutation = BytesUtil.permutation(arrayBits, permutationValues);
