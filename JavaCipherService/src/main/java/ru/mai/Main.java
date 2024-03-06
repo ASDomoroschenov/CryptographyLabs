d26665e4ece3171710dda715388c08fcd772fce5
ASDomoroschenov <sashachicha228@mail.ru>
2024-03-07 01:55:49 +0300

    refactor code

--- JavaCipherService/src/main/java/ru/mai/Main.java
+++ JavaCipherService/src/main/java/ru/mai/Main.java
@@ -2,11 +2,8 @@ package ru.mai;
 
 import lombok.extern.slf4j.Slf4j;
 import ru.mai.encryption.encryption_impl.DES.DES;
-import ru.mai.utils.BitsUtil;
-import ru.mai.utils.BytesUtil;
 
 import java.util.Arrays;
-import java.util.Objects;
 
 @Slf4j
 public class Main {
