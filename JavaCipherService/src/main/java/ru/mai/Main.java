bf652608430879e18daccb8185c55df97d923346
ASDomoroschenov <sashachicha228@mail.ru>
2024-03-10 16:01:10 +0300

    refactor

--- JavaCipherService/src/main/java/ru/mai/Main.java
+++ JavaCipherService/src/main/java/ru/mai/Main.java
@@ -6,14 +6,11 @@ import ru.mai.cipher.cipher_service.CipherService.CipherAlgorithm;
 import ru.mai.cipher.cipher_service.CipherService.EncryptionMode;
 import ru.mai.cipher.cipher_service.CipherService.StuffingMode;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.Arrays;
 
 @Slf4j
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         byte[] key = {1, 2, 3, 4, 5, 6, 7};
         byte[] text = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
