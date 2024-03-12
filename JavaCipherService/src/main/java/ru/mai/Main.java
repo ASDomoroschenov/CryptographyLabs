8327b1aec2a82df88c76a371cfbe343e396fe3e8
ASDomoroschenov <sashachicha228@mail.ru>
2024-03-13 00:01:57 +0300

    finish

--- JavaCipherService/src/main/java/ru/mai/Main.java
+++ JavaCipherService/src/main/java/ru/mai/Main.java
@@ -15,38 +15,24 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) throws IOException {
         byte[] keyDEAL = {1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8};
-        byte[] keyDES = {1, 2, 3, 4, 5, 6, 7, 8};
-        byte[] text = {1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8};
+
         CipherService service = new CipherService(
                 keyDEAL,
                 CipherAlgorithm.DEAL,
-                EncryptionMode.RD,
+                EncryptionMode.ECB,
                 StuffingMode.ANSI_X_923,
                 keyDEAL);
-        byte[] encrypt = service.encrypt(text);
-        byte[] decrypt = service.decrypt(encrypt);
 
-        System.out.println(Arrays.toString(decrypt));
+        long begin = System.currentTimeMillis();
+        String source = "/home/alexandr/CryptographyLabs/JavaCipherService/src/main/resources/ru/mai/test/input/text.txt";
+        String encrypt = service.encrypt(source);
+        String decrypt = service.decrypt(encrypt);
 
-//        byte[] key = {1, 2, 3, 4, 5, 6, 7, 8};
-//
-//        CipherService service = new CipherService(
-//                key,
-//                CipherAlgorithm.DES,
-//                EncryptionMode.ECB,
-//                StuffingMode.ANSI_X_923,
-//                new byte[]{1, 2, 3, 4, 5, 6, 7, 8});
-//
-//        long begin = System.currentTimeMillis();
-//        String source = "/home/alexandr/CryptographyLabs/JavaCipherService/src/main/resources/ru/mai/test/input/text.txt";
-//        String encrypt = service.encrypt(source);
-//        String decrypt = service.decrypt(encrypt);
-//
-//        System.out.println(Arrays.equals(
-//                Files.readAllBytes(Paths.get(source)),
-//                Files.readAllBytes(Paths.get(decrypt))
-//        ));
-//
-//        System.out.println(System.currentTimeMillis() - begin);
+        System.out.println(Arrays.equals(
+                Files.readAllBytes(Paths.get(source)),
+                Files.readAllBytes(Paths.get(decrypt))
+        ));
+
+        System.out.println(System.currentTimeMillis() - begin);
     }
 }
