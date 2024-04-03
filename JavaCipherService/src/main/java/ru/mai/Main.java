e2c4205a3e1edf51cb3fb2b8f094a28a3449a427
ASDomoroschenov <sashachicha228@mail.ru>
2024-04-03 18:14:16 +0300

    change BigInteger[] to Pair in FermatAttack

--- JavaCipherService/src/main/java/ru/mai/Main.java
+++ JavaCipherService/src/main/java/ru/mai/Main.java
@@ -4,12 +4,11 @@ import lombok.extern.slf4j.Slf4j;
 import ru.mai.RSA.attack.FermatAttack;
 
 import java.math.BigInteger;
-import java.util.Arrays;
 
 @Slf4j
 public class Main {
     public static void main(String[] args) {
         FermatAttack fermatAttack = new FermatAttack();
-        System.out.println(Arrays.toString(fermatAttack.attack(new BigInteger("7"), new BigInteger("21"))));
+        System.out.println(fermatAttack.attack(new BigInteger("7"), new BigInteger("21")));
     }
 }
--- JavaCipherService/src/main/java/ru/mai/RSA/attack/FermatAttack.java
+++ JavaCipherService/src/main/java/ru/mai/RSA/attack/FermatAttack.java
@@ -1,11 +1,12 @@
 package ru.mai.RSA.attack;
 
+import org.apache.commons.lang3.tuple.Pair;
 import ru.mai.service.ModuloService;
 
 import java.math.BigInteger;
 
 public class FermatAttack {
-    public BigInteger[] attack(BigInteger e, BigInteger N) {
+    public Pair<BigInteger, BigInteger> attack(BigInteger e, BigInteger N) {
         BigInteger nSqrt = N.sqrt().add(BigInteger.ONE);
         BigInteger xSqrt = nSqrt.pow(2).subtract(N);
 
@@ -19,10 +20,7 @@ public class FermatAttack {
         BigInteger eulerFunction = p.subtract(BigInteger.ONE).multiply(q.subtract(BigInteger.ONE));
         BigInteger d = ModuloService.gcdExtended(e, eulerFunction)[1];
 
-        return new BigInteger[] {
-                d,
-                eulerFunction
-        };
+        return Pair.of(d, eulerFunction);
     }
 
     private boolean isSquare(BigInteger number) {
