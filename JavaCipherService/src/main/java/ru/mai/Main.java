efd3fa4092709eac6846e89f63649a716bfc1773
ASDomoroschenov <sashachicha228@mail.ru>
2024-04-03 16:20:05 +0300

    fermat attack

--- JavaCipherService/src/main/java/ru/mai/Main.java
+++ JavaCipherService/src/main/java/ru/mai/Main.java
@@ -1,38 +1,16 @@
 package ru.mai;
 
 import lombok.extern.slf4j.Slf4j;
-import ru.mai.RSA.RSA;
+import ru.mai.RSA.attack.FermatAttack;
 
 import java.io.IOException;
 import java.math.BigInteger;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.Arrays;
-import java.util.Random;
-
-import static ru.mai.RSA.RSA.PrimeTest.MILLER_RABIN;
 
 @Slf4j
 public class Main {
     public static void main(String[] args) throws IOException {
-        RSA rsa = new RSA(MILLER_RABIN, 0.999, new Random().nextInt(200) + 50);
-        BigInteger[][] keys = rsa.getKey();
-        BigInteger e = keys[0][0];
-        BigInteger d = keys[1][0];
-        BigInteger N = keys[1][1];
-
-        try {
-            String pathToInputFile = "/home/alexandr/CryptographyLabs/JavaCipherService/src/main/resources/image.jpg";
-            String encryptFile = rsa.encryptFile(pathToInputFile, e, N);
-            String decryptFile = rsa.decryptFile(encryptFile, d, N);
-
-            System.out.println(Arrays.equals(
-                    Files.readAllBytes(Path.of(pathToInputFile)),
-                    Files.readAllBytes(Path.of(decryptFile)))
-            );
-        } catch (Exception ex) {
-            log.error(ex.getMessage());
-            log.error(Arrays.toString(ex.getStackTrace()));
-        }
+        FermatAttack fermatAttack = new FermatAttack();
+        System.out.println(Arrays.toString(fermatAttack.attack(new BigInteger("5"), new BigInteger("21"))));
     }
 }
